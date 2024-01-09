031a5d2f15ea4f8bf54a10888b8680c2ad3ce065
robbiebmorris <robbiebmorris@gmail.com>
2024-01-09 14:13:34 +0000

    [FIN]: Finished project

--- tableau.py
+++ tableau.py
@@ -1,4 +1,3 @@
-
 MAX_CONSTANTS = 10
 
 # propositional
@@ -56,7 +55,6 @@ def parse_propositional_formula(fmla):
     if (fmla[0] == '~'):
           return Negation(parse_propositional_formula(fmla[1:]))
     elif fmla[0] == '(' and fmla[-1] == ')':
-        # Find the main connective
         count = 0
         for i in range(1, len(fmla) - 1):
             if fmla[i] == '(':
@@ -92,13 +90,9 @@ def parse_first_order_formula(fmla):
     elif fmla[0] in QUANTIFIERS:
         return Quantifier(fmla[0], fmla[1], parse_first_order_formula(fmla[2:]))
     elif fmla[0] in PREDICATES and fmla[1] == '(' and fmla[-1] == ')':
-        #find the first ',' in fmla:
         index = fmla.find(',')
-        # print(fmla[2:index])
-        # print(fmla[index+1:-1])
         return Predicate(fmla[0], parse_first_order_formula(fmla[2:index]), parse_first_order_formula(fmla[index+1:-1]))
     elif fmla[0] == '(' and fmla[-1] == ')':
-        # Find the main connective
         count = 0
         for i in range(1, len(fmla) - 1):
             if fmla[i] == '(':
@@ -120,16 +114,13 @@ def check_if_valid_fol_formula(fmla):
     elif fmla.__class__.__name__ == 'Negation':
         return check_if_valid_fol_formula(fmla.formula)
     elif (fmla.__class__.__name__ == 'Quantifier' and fmla.quantifier in QUANTIFIERS and fmla.variable in FOL_LETTERS):
-        #TODO: need to add variable from quantifier to set of fol variables
         return check_if_valid_fol_formula(fmla.formula)
     elif fmla.__class__.__name__ == 'BinaryConnective' or fmla.__class__.__name__ == "Predicate":
         return check_if_valid_fol_formula(fmla.left) and check_if_valid_fol_formula(fmla.right)
     else:
         return False
       
-      
-# print(check_if_valid_propositional_formula(parse_propositional_formula("(q\/p)")))
-# print(check_if_valid_fol_formula(parse_first_order_formula("ExEy((Q(x,x)/\Q(y,y))\/)")))
+
 
 def parse(fmla):
     
@@ -158,30 +149,83 @@ def parse(fmla):
         elif (fol.__class__.__name__ == 'BinaryConnective'):
             return 5
     return 0
-      
-# print(parse_propositional_formula('(p\\/(r\\/s))'))
 
 # Return the LHS of a binary connective formula
 def lhs(fmla):
-    return parse_propositional_formula(fmla).left
+    fol = parse_first_order_formula(fmla)
+    prop = parse_propositional_formula(fmla)
+    if (check_if_valid_fol_formula(fol)):
+      return fol.left
+    elif (check_if_valid_propositional_formula(prop)):
+      return prop.left
+    else:
+      return None
 
 # Return the connective symbol of a binary connective formula
 def con(fmla):
-    return parse_propositional_formula(fmla).connective
+    fol = parse_first_order_formula(fmla)
+    prop = parse_propositional_formula(fmla)
+    if (check_if_valid_fol_formula(fol)):
+      return fol.connective
+    elif (check_if_valid_propositional_formula(prop)):
+      return prop.connective
+    else:
+      return None
 
 # Return the RHS symbol of a binary connective formula
 def rhs(fmla):
-    return parse_propositional_formula(fmla).right
+    fol = parse_first_order_formula(fmla)
+    prop = parse_propositional_formula(fmla)
+    if (check_if_valid_fol_formula(fol)):
+      return fol.right
+    elif (check_if_valid_propositional_formula(prop)):
+      return prop.right
+    else:
+      return None
+    
+def substitute_literal(fmla, old, new):
+    if fmla.__class__.__name__ == 'Proposition' and fmla.name == old:
+        return Proposition(new)
+    elif fmla.__class__.__name__ == 'Negation':
+        return Negation(substitute_literal(fmla.formula, old, new))
+    elif fmla.__class__.__name__ == 'BinaryConnective':
+        return BinaryConnective(substitute_literal(fmla.left, old, new), substitute_literal(fmla.right, old, new), fmla.connective)
+    elif fmla.__class__.__name__ == 'Quantifier':
+        if fmla.variable == old:
+            return Quantifier(fmla.quantifier, new, substitute_literal(fmla.formula, old, new))
+        return Quantifier(fmla.quantifier, fmla.variable, substitute_literal(fmla.formula, old, new))
+    elif fmla.__class__.__name__ == 'Predicate':
+        return Predicate(fmla.predicate, substitute_literal(fmla.left, old, new), substitute_literal(fmla.right, old, new))
+    else:
+        return fmla
 
+def delta_expansion(node, used_chars):
+    # print("delta expansion")
+    #find a new constant not in formula
+    delta = 'a'
+    while delta in used_chars or node.variable == delta:
+        delta = chr(ord(delta) + 1)
+    used_chars.add(delta)
+    return [substitute_literal(node.formula, node.variable, delta)]
 
-def expand_node(node):
+def gamma_expansion(node, c):
+    #replace all x with gamma
+    return substitute_literal(node, node.variable, c).formula
+
+def expand_node(node, used_chars, gamma_q):
     # use the tableau rules to expand the node based on the type of formula it is
-    if (node.__class__.__name__ == 'Proposition'):
+    if (node.__class__.__name__ == "Quantifier"):
+        if (node.quantifier == 'E'):
+            return delta_expansion(node, used_chars)
+        elif (node.quantifier == 'A'):
+            gamma_q.append(node)
+            return []
+    elif node.__class__.__name__ == 'Predicate' or node.__class__.__name__ == 'Proposition':
         return [node]
     elif (node.__class__.__name__ == 'Negation'):
         #check for ~(p/\q) and ~(p\/q)
         if (node.formula.__class__.__name__ == 'BinaryConnective'):
-            # ~(p/\q) -> (~p) , (~q)
+            #~(p/\q) -> (~p) , (~q)
             if (node.formula.connective == '/\\'):
                 return ([Negation(node.formula.left)], [Negation(node.formula.right)])
             elif (node.formula.connective == '\\/'):
@@ -191,8 +235,13 @@ def expand_node(node):
         #check for ~~p
         elif (node.formula.__class__.__name__ == 'Negation'):
             return [node.formula.formula] 
-
-        #check for ~p
+        #~E -> A~
+        elif (node.formula.__class__.__name__ == "Quantifier" and node.formula.quantifier == 'E'):
+            gamma_q.append(Quantifier('A', node.formula.variable, Negation(node.formula.formula)), used_chars)
+            return []
+        #~A -> E~
+        elif (node.formula.__class__.__name__ == "Quantifier" and node.formula.quantifier == 'A'):
+            return delta_expansion(Quantifier('E', node.formula.variable, Negation(node.formula.formula)), used_chars)
         else:
             return [node]
     elif (node.__class__.__name__ == 'BinaryConnective'):
@@ -206,7 +255,6 @@ def expand_node(node):
         elif (node.connective == '=>'):
             return ([Negation(node.left)], [node.right])
     else:
-        print("something is broken")
         return
 
 
@@ -215,7 +263,11 @@ def is_closed(branch):
     propSet = set()
     negSet = set()
     for node in branch:
-        if (node.__class__.__name__ == 'Proposition'):
+        if (node.__class__.__name__ == 'Predicate'):
+            propSet.add(str(node))
+        elif (node.__class__.__name__ == 'Negation' and node.formula.__class__.__name__ == 'Predicate'):
+            negSet.add(str(node.formula))
+        elif (node.__class__.__name__ == 'Proposition'):
             propSet.add(node.name)
         elif (node.__class__.__name__ == 'Negation' and node.formula.__class__.__name__ == 'Proposition'):
             negSet.add(node.formula.name)
@@ -225,70 +277,58 @@ def is_closed(branch):
 def is_leaf_branch(branch):
     #checks whether a branch is a leaf branch (contains something larger than a prop or neg prop)
     for node in branch:
-        if (node.__class__.__name__ == 'Negation' and (node.formula.__class__.__name__ == 'BinaryConnective' or node.formula.__class__.__name__ == 'Negation')):
+        if (node.__class__.__name__ == 'Negation' and not (node.formula.__class__.__name__ == 'Proposition' or node.formula.__class__.__name__ == 'Predicate')):
             return False
-        if (node.__class__.__name__ == 'BinaryConnective'):
+        elif (node.__class__.__name__ == 'BinaryConnective' or node.__class__.__name__ == 'Quantifier'):
             return False
     return True
-  
-  
-def check_number_of_letters(fmla):
-    #checks whether the number of constants in a formula is less than or equal to MAX_CONSTANTS
-    if (fmla.__class__.__name__ == 'Proposition'):
-        return 1
-    elif (fmla.__class__.__name__ == 'Negation'):
-        return check_number_of_letters(fmla.formula)
-    else:
-        return check_number_of_letters(fmla.left) + check_number_of_letters(fmla.right)
-    
-    
-# print(check_number_of_letters(parse_propositional_formula('~~((p\/q)/\((p=>~p)/\(~p=>p)))')))
+
   
 # You may choose to represent a theory as a set or a list
 def theory(fmla):
-  return parse_propositional_formula(fmla)
-  
+  fol = parse_first_order_formula(fmla)
+  prop = parse_propositional_formula(fmla)
+  if (check_if_valid_fol_formula(fol)):
+    return fol
+  elif (check_if_valid_propositional_formula(prop)):
+    return prop
+  else:
+    return None
+
   
 def sat(tableau):
-    # if (check_number_of_letters(tableau[0]) > MAX_CONSTANTS):
-    #     #too long
-    #     return 2
-    
-    branches = [tableau]
-    while len(branches) > 0:
 
-        # for node in branches:
-        #     [print("node: ", n) for n in node]
-        #     print("\n")
-        # [print("b: ", b) for b in branch]
-        # print("leaf branch: ", is_leaf_branch(branch))
-        # print("branch closed: ", is_closed(branch))
-        
+    branches = [tableau]
+    used = set()
+    gamma_q = []
+    while len(branches) > 0:
+        if (len(used) > MAX_CONSTANTS):
+            return 2
 
         branch = branches.pop()
 
         if is_closed(branch):
-            #get rid of all sub branches of this branch from branches
             continue
-        elif is_leaf_branch(branch) and not is_closed(branch):
+        elif is_leaf_branch(branch) and not is_closed(tableau):
             #satisfiable
             return 1
         else:
             sub_branches = []
+            sub_gamma_q = []
+            sub_used = used.copy()
             for node in branch:
-                expansion = expand_node(node)
+
+                expansion = expand_node(node, used, sub_gamma_q)
                 #alpha expansion (p/\q, ~(p\/q), ~(p=>q))
-                if type(expansion) is not tuple:
-                    # [print("e: ", e) for e in expansion]
+                if type(expansion) is list:
                     if len(sub_branches) > 0:
                         for i in range(len(sub_branches)):
-                          sub_branches[i] += expansion
+                            sub_branches[i] += expansion
                     else:
                         sub_branches.append(expansion)
-          
+
                 #beta expansion (p\/q, ~(p/\q), (p=>q))
-                else:
-                    # [print("tup e: ", e[0]) for e in expansion]
+                elif type(expansion) is tuple:
                     # if there was a previous alpha expansion, remove it and combine with new expansion
                     if (len(sub_branches) == 1):
                         temp = sub_branches.pop()
@@ -296,41 +336,38 @@ def sat(tableau):
                         sub_branches.append(expansion[1] + temp)
                     # if there was a previous beta expansion, combine with new expansion
                     elif (len(sub_branches) == 2):
-                        sub_branches[0] += expansion[0]
-                        sub_branches[1] += expansion[1]
+                        sub_branches[0] += [node]
+                        sub_branches[1] += [node]
                     else:
                         sub_branches.append(expansion[0])
                         sub_branches.append(expansion[1])
-                    
-                    # print("added from beta:" , expansion[0][0])
-                    # print("added from beta:" , expansion[1][0])
-   
-            branches += sub_branches
-            # count =0
-            # print(sub_branches)
-            # for b in sub_branches:
-            #     [print(f"sub b {count}: ", k) for k in b]
-            #     count += 1
-            # print(branches)
 
+            for g in sub_gamma_q:
+                for c in used:
+                    exp = gamma_expansion(g, c)
+                    for b in sub_branches:
+                        b.append(exp)
+
+            dif = used.difference(sub_used)
+            
+            for g in gamma_q:
+                if g:
+                  for c in dif:
+                    exp = gamma_expansion(g, c)
+                    for b in sub_branches:
+                        b.append(exp)
+
+            gamma_q += sub_gamma_q
+            
+            for b in sub_branches:
+                branches.append(b)
+            
     #not satisfiable
     return 0
 
-# print(sat([parse_propositional_formula('(q/\~(q/\(p/\q)))')]))
-# print(sat([parse_propositional_formula('~((p=>p)=>(q=>q))')]))
-# print(sat([parse_propositional_formula('~~((p\/q)/\((p=>~p)/\(~p=>p)))')]))
 
-
-# def sat(tableau):
-#   #write code using tableau rules to check if the formula is satisfiable
-#   #return 0 if not satisfiable, 1 if satisfiable, 2 if may or may not be satisfiable
-#   #input is the output from theory function above
-  
-# print(check_if_valid_propositional_formula(parse_propositional_formula('(p=>q)')))
-
-f = open('./testinputs/testinput2/input.txt')
 #DO NOT MODIFY THE CODE BELOW
-# f = open('input.txt')
+f = open('input.txt')
 
 parseOutputs = ['not a formula',
                 'an atom',
